bffb1c5691422ccd5938dabd2416d5a77ce8834e
fengjianglin <fengjianglin6817@dingtalk.com>
2018-08-10 09:35:13 +0800

    update

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,10 +2,21 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+using namespace std;
 
 
 int main()
 {
+	char name[] = "appale";
+	cout << name<<endl;
+	char *sex = "nana";
+	cout << sex<<endl;
+	char *ps = sex;
+	cout << ps<<endl;
+	int a = 10;
+	int* pa = &a;
+	cout << pa;
     return 0;
 }
 
